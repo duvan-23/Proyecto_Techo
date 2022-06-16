e9d03311c13909387c5e128ade6b6b816f6b2a0f
Duvan Caicedo <duvamendi2@hotmail.com>
2022-06-15 22:30:32 -0500

    ajuste para merge

--- techo/src/App.js
+++ techo/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 
 import { MainPage } from './components/MainPage/MainPage';
 import { NavBar } from './components/NavBar/NavBar';
-
+import Herramientas from './components/herramientas/herramientas';
 
 function App() {
   return (
     <div>
       <NavBar/>
       <MainPage/>
+      <Herramientas/>
     </div>
   );
 }
